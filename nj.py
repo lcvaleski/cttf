c19ad1454644a48f3212ef29204f71bbb6f771a2
Windows <loganvaleski@gmail.com>
2021-06-14 11:44:53 -0600

    Figured out that the page needs to load

--- nj.py
+++ nj.py
@@ -2,13 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
-driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
-driver.get('https://njhealth.maps.arcgis.com/apps/MapSeries/index.html?appid=50c2c6af93364b4da9c0bf6327c04b45&folderid=e5d6362c0f1f4f9684dc650f00741b24')
+driver = webdriver.Chrome(executable_path='C:/Users/Logan/Desktop/chromedriver.exe')
+driver.get('https://njhealth.maps.arcgis.com/apps/dashboards/2ec0d7d96a18451bb60acfed3e76e563')
+time.sleep(10)
 
-WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div[1]/ul/li[2]/button")))
-print("Here")
-driver.find_elements_by_xpath("/html/body/div[3]/div/div[1]/ul/li[2]/button").click()
-casespath = "/html/body/div/div/div/div[2]/div/div/div/margin-container/full-container/div[9]/margin-container/full-container/div/div/div/div[2]/svg/g[2]/text"
-cases = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, casespath)))
-print(cases)
+#WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div/div[2]/div/div/div/margin-container/full-container/div[10]/margin-container/full-container/div/div/div/div[2]/svg/g[2]/text")))
+data = driver.find_elements_by_tag_name("text")
+for e in data:
+    print(str(e.get_attribute("innerHTML").strip()))
+driver.quit()

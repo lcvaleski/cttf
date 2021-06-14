58f1f196ce16d81e7e45ed004ccb5395aa692548
Logan Valeski <loganvaleski@gmail.com>
2021-06-14 12:47:59 -0600

    Trying out color stuff

--- nj.py
+++ nj.py
@@ -4,12 +4,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-driver = webdriver.Chrome(executable_path='C:/Users/Logan/Desktop/chromedriver.exe')
+driver = webdriver.Chrome()
 driver.get('https://njhealth.maps.arcgis.com/apps/dashboards/2ec0d7d96a18451bb60acfed3e76e563')
 time.sleep(10)
 
-#WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div/div[2]/div/div/div/margin-container/full-container/div[10]/margin-container/full-container/div/div/div/div[2]/svg/g[2]/text")))
 data = driver.find_elements_by_tag_name("text")
 for e in data:
-    print(str(e.get_attribute("innerHTML").strip()))
+    if str(e.getCssValue("color") == '002673':
+        print(str(e.get_attribute("innerHTML").strip()))
+        break
 driver.quit()
